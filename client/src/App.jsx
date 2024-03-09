96ae0b2309cdd8ee9016ee5de472feefd850ab6c
Abdul <rahman778@gmail.com>
2024-03-09 22:19:12 +0530

    setup react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+
+import router from "./router";
 
 function App() {
-   return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+   return <RouterProvider router={router} />;
 }
 
 export default App;
